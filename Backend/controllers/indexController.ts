e65447a2086c0239c3beeb5b85747f8f08130f74
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-03-17 17:14:53 +1300

    added login route

--- Backend/controllers/indexController.ts
+++ Backend/controllers/indexController.ts
@@ -7,7 +7,7 @@ import { prisma } from "../lib/prisma.js";
 const emailLengthErr = "must be between 1 and 50 characters";
 const lengthErrShort = "must be between 1 and 25 characters";
 const passwordAlphaNumericErr = "must contain at least a letter and a number";
-const emailErr = "Must be a valid email Adress"
+const emailErr = "Must be a valid email Address"
 
 const userSchema = z.object({
   //parses the object to the schema, narrowing the types that are allowed
@@ -16,6 +16,10 @@ const userSchema = z.object({
   email: z.email({message: emailErr}).max(25, { message: `Email: ${emailLengthErr}` }).trim()
 });
 
+const logInUserSchema = z.object({
+  email: z.email(),
+  password: z.string({message: emailErr}),
+});
 
 export async function signUp (req: Request, res: Response, next: NextFunction) {
     const user = {
@@ -49,4 +53,63 @@ export async function signUp (req: Request, res: Response, next: NextFunction) {
     }
 }
 
-export async function logIn
+export async function logIn(req: Request, res: Response, next: NextFunction) {
+    const user = {
+    email: req.body["email"],
+    password: req.body["password"],
+  };
+
+  const parsedUser = logInUserSchema.parse(user);
+
+  if (typeof parsedUser.email != "string") {
+    return res.status(400).json({ message: "Incorrect header" });
+  }
+
+  try {
+    const userCheck = await prisma.users.findUnique({
+      where: {
+        email: parsedUser.email,
+      },
+    });
+
+    if (!userCheck) {
+      return res.status(400).json({ message: "Incorrect username" });
+    }
+
+    const match = await bcrypt.compare(parsedUser.password, userCheck.password);
+    if (!match) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
+    const secret = process.env.JWT_SECRET;
+
+    if (!secret) {
+      throw new Error("JWT secret is not defined");
+    }
+
+    jwt.sign(
+      { id: userCheck.id, username: userCheck.username },
+      secret,
+      { expiresIn: '1w' },
+      function (err, token) {
+        if (err || !token) {
+          //need to account for an error or no token
+          return res.status(500).json({ message: "Token generation failed" });
+        }
+        return res.json({
+          message: "Successfully logged in",
+          token,
+          username: userCheck.username,
+        });
+      },
+    );
+  } catch (error) {
+    res.sendStatus(500);
+    if (error instanceof ZodError) { //if error is a zod error send back
+            return res.status(400).json({
+                errors: error.issues
+            });
+        };
+    return error;
+  }
+}
