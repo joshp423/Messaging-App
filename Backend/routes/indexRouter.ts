ce77f0f8f0019af673412ec32bc4650ae4c5d4d1
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-03-19 14:58:46 +1300

    added functions to router

--- Backend/routes/indexRouter.ts
+++ Backend/routes/indexRouter.ts
@@ -4,8 +4,9 @@ const indexRouter = Router();
 
 indexRouter.post("/sign-up", indexController.signUp);
 indexRouter.post("/log-in", indexController.logIn);
-indexRouter.put("/edit-profile", indexController.editProfile);
-indexRouter.post("/send-message-solo", indexController.sendMessageSingleRecipient);
-indexRouter.post("/send-message-group", indexController.sendMessageGroupRecipient);
-
+indexRouter.put("/edit-profile", indexController.verifyToken, indexController.editProfile);
+indexRouter.post("/send-message-solo", indexController.verifyToken, indexController.sendMessageSingleRecipient);
+indexRouter.post("/send-message-group", indexController.verifyToken, indexController.sendMessageGroupRecipient);
+indexRouter.get("/receive-messages", indexController.verifyToken, indexController.receiveMessages);
+indexRouter.put("/create-group", indexController.verifyToken, indexController.createNewGroup);
 export default indexRouter;
