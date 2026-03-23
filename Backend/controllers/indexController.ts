495eff47750281d997bac6ad01c00bf70ed201d0
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-03-23 19:18:23 +1300

    ran prettier, tidied up prisma setup

--- Backend/controllers/indexController.ts
+++ Backend/controllers/indexController.ts
@@ -2,9 +2,9 @@ import bcrypt from "bcryptjs";
 import { z, ZodError } from "zod";
 import jwt from "jsonwebtoken";
 import type { Request, Response, NextFunction } from "express";
-import { prisma } from "../lib/prisma.js";
+import prisma from "../lib/prisma.js";
 
-const emailLengthErr = "must be between 1 and 50 characters";
+const emailLengthErr = "must be between 1 and 254 characters";
 const lengthErrShort = "must be between 1 and 25 characters";
 const passwordAlphaNumericErr = "must contain at least a letter and a number";
 const emailErr = "Must be a valid email Address";
@@ -24,13 +24,13 @@ const userSignUpSchema = z.object({
     .regex(/^(?=.*[A-Za-z])(?=.*\d).+$/, { message: passwordAlphaNumericErr }),
   email: z
     .email({ message: emailErr })
-    .max(25, { message: `Email: ${emailLengthErr}` })
+    .max(254, { message: `Email: ${emailLengthErr}` })
     .trim(),
 });
 
 const userLogInSchema = z.object({
   email: z.email(),
-  password: z.string({ message: emailErr }),
+  password: z.string(),
 });
 
 const userEditProfileSchema = z.object({
@@ -49,25 +49,26 @@ const userMessageSingleSchema = z.object({
   receiverId: z.number(),
   message: z.string().trim(),
   imageUrl: z.string(),
-})
+});
 
 const userMessageGroupSchema = z.object({
   senderId: z.number(),
   groupId: z.number(),
   message: z.string().trim(),
   imageUrl: z.string(),
-})
+});
 
 const userGroupSchema = z.object({
   userIds: z.array(z.number()),
-  name: z.string().trim().max(25, ({ message: `Group name: ${lengthErrShort}`}))
-})
+  name: z
+    .string()
+    .trim()
+    .max(25, { message: `Group name: ${lengthErrShort}` }),
+});
 
 const userIdSchema = z.object({
-  id: z.number()
-})
-
-
+  id: z.number(),
+});
 
 export async function signUp(req: Request, res: Response, next: NextFunction) {
   const user = {
@@ -75,14 +76,16 @@ export async function signUp(req: Request, res: Response, next: NextFunction) {
     email: req.body["email"],
     password: req.body["password"],
   };
-  const parsedUser = userSignUpSchema.parse(user);
-
-  if (typeof parsedUser.username != "string") {
-    return res.status(400).json({ message: "Incorrect header" });
-  }
 
   try {
+    const parsedUser = userSignUpSchema.parse(user);
+
+    if (typeof parsedUser.username != "string") {
+      return res.status(400).json({ message: "Incorrect header" });
+    }
+
     const hashedPassword = await bcrypt.hash(parsedUser.password, 10);
+
     await prisma.users.create({
       data: {
         username: parsedUser.username,
@@ -108,13 +111,12 @@ export async function logIn(req: Request, res: Response) {
     password: req.body["password"],
   };
 
-  const parsedUser = userLogInSchema.parse(user);
-
-  if (typeof parsedUser.email != "string") {
-    return res.status(400).json({ message: "Incorrect header" });
-  }
-
   try {
+    const parsedUser = userLogInSchema.parse(user);
+
+    if (typeof parsedUser.email != "string") {
+      return res.status(400).json({ message: "Incorrect header" });
+    }
     const userCheck = await prisma.users.findUnique({
       where: {
         email: parsedUser.email,
@@ -185,11 +187,14 @@ export async function editProfile(req: Request, res: Response) {
   }
 }
 
-export async function sendMessageSingleRecipient(req: Request, res: Response, next: NextFunction) {
+export async function sendMessageSingleRecipient(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   try {
-    const { senderId, receiverId, message, imageUrl } = userMessageSingleSchema.parse(
-      req.body,
-    );
+    const { senderId, receiverId, message, imageUrl } =
+      userMessageSingleSchema.parse(req.body);
 
     await prisma.messagesSolo.create({
       data: {
@@ -211,11 +216,14 @@ export async function sendMessageSingleRecipient(req: Request, res: Response, ne
   }
 }
 
-export async function sendMessageGroupRecipient(req: Request, res: Response, next: NextFunction) {
+export async function sendMessageGroupRecipient(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   try {
-    const { senderId, groupId, message, imageUrl } = userMessageGroupSchema.parse(
-      req.body,
-    );
+    const { senderId, groupId, message, imageUrl } =
+      userMessageGroupSchema.parse(req.body);
 
     await prisma.messagesGroup.create({
       data: {
@@ -237,18 +245,20 @@ export async function sendMessageGroupRecipient(req: Request, res: Response, nex
   }
 }
 
-
-export async function createNewGroup(req: Request, res: Response, next: NextFunction) {
+export async function createNewGroup(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   try {
-    const { userIds, name } = userGroupSchema.parse(
-      req.body,
-    );
+    const { userIds, name } = userGroupSchema.parse(req.body);
 
-    await prisma.groups.create({ // create new group and link it to users by id
+    await prisma.groups.create({
+      // create new group and link it to users by id
       data: {
         name,
         users: {
-          connect: userIds.map((id: number) => ({ id })) // array becomes [{ id: number }] objects - go to users and find id =
+          connect: userIds.map((id: number) => ({ id })), // array becomes [{ id: number }] objects - go to users and find id =
         },
       },
     });
@@ -298,29 +308,25 @@ export function verifyToken(
 
 export async function receiveMessages(req: Request, res: Response) {
   try {
-    const { id } = userIdSchema.parse(
-      req.body,
-    );
+    const { id } = userIdSchema.parse(req.body);
 
     const messagesSolo = await prisma.messagesSolo.findMany({
       orderBy: {
-        timeSent: "asc"
-      }
+        timeSent: "asc",
+      },
     });
 
     const messagesGroup = await prisma.messagesGroup.findMany({
       orderBy: {
-        timeSent: "asc"
-      }
+        timeSent: "asc",
+      },
     });
 
     return res.status(200).json({
-      messagesSolo, messagesGroup
-    })
-  } catch (error){
+      messagesSolo,
+      messagesGroup,
+    });
+  } catch (error) {
     return res.status(500).json({ error });
   }
 }
-
-
-
--- Backend/routes/indexRouter.ts
+++ Backend/routes/indexRouter.ts
@@ -4,9 +4,29 @@ const indexRouter = Router();
 
 indexRouter.post("/sign-up", indexController.signUp);
 indexRouter.post("/log-in", indexController.logIn);
-indexRouter.put("/edit-profile", indexController.verifyToken, indexController.editProfile);
-indexRouter.post("/send-message-solo", indexController.verifyToken, indexController.sendMessageSingleRecipient);
-indexRouter.post("/send-message-group", indexController.verifyToken, indexController.sendMessageGroupRecipient);
-indexRouter.get("/receive-messages", indexController.verifyToken, indexController.receiveMessages);
-indexRouter.put("/create-group", indexController.verifyToken, indexController.createNewGroup);
+indexRouter.put(
+  "/edit-profile",
+  indexController.verifyToken,
+  indexController.editProfile,
+);
+indexRouter.post(
+  "/send-message-solo",
+  indexController.verifyToken,
+  indexController.sendMessageSingleRecipient,
+);
+indexRouter.post(
+  "/send-message-group",
+  indexController.verifyToken,
+  indexController.sendMessageGroupRecipient,
+);
+indexRouter.get(
+  "/receive-messages",
+  indexController.verifyToken,
+  indexController.receiveMessages,
+);
+indexRouter.put(
+  "/create-group",
+  indexController.verifyToken,
+  indexController.createNewGroup,
+);
 export default indexRouter;
